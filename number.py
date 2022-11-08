98780709a43e99f87ea3e34e7f753f98d5e27f0a
m-dhal <manish@lukadvisor.tech>
2022-11-08 20:30:40 +0800

    updated number plate processing & face detection

--- number.py
+++ number.py
@@ -15,6 +15,9 @@ import pandas as pd
 import os.path
 from local_utils import detect_lp
 from tensorflow.keras.models  import model_from_json
+from tensorflow.keras.models import load_model as load_keras_model
+from tensorflow.keras.applications.mobilenet_v2 import preprocess_input
+from tensorflow.keras.preprocessing.image import img_to_array
 from os.path import splitext,basename
 from PIL import Image
 
@@ -22,24 +25,74 @@ from PIL import Image
 import streamlit as st 
 
 
+# ------------------------- FACE DETECTION MASK On&Off
+def doMaskOnOffDetection(opencv_image):
+    prototxtPath = os.path.sep.join(["face_detector", "deploy.prototxt"])
+    weightsPath = os.path.sep.join(["face_detector",
+                                    "res10_300x300_ssd_iter_140000.caffemodel"])
+    net = cv2.dnn.readNet(prototxtPath, weightsPath)
 
+    model = load_keras_model("mask_detector.model")
+    st.text("Face Detection Performed")
 
-# Face Detection using Haar Cascade 
-def doFaceDetection(img):
-    # Load the cascade
-    face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+    image = opencv_image
+    (h, w) = image.shape[:2]
 
-    # Convert into grayscale
-    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    # construct a blob from the image
+    blob = cv2.dnn.blobFromImage(image, 1.0, (300, 300),
+                                 (104.0, 177.0, 123.0))
+
+    # pass the blob through the network and obtain the face detections
+    print("[INFO] computing face detections...")
+    net.setInput(blob)
+    detections = net.forward()
     
-    # Detect faces
-    faces = face_cascade.detectMultiScale(gray, 1.3, 5)
-    # Draw rectangle around the faces
-    for (x, y, w, h) in faces:
-        cv2.rectangle(img, (x, y), (x+w, y+h), (255, 0, 0), 2)
-    
-    return img
 
+    # loop over the detections
+    for i in range(0, detections.shape[2]):
+        # extract the confidence (i.e., probability) associated with
+        # the detection
+        confidence = detections[0, 0, i, 2]
+
+        # filter out weak detections by ensuring the confidence is
+        # greater than the minimum confidence
+        if confidence > 0.5:
+            # compute the (x, y)-coordinates of the bounding box for
+            # the object
+            box = detections[0, 0, i, 3:7] * np.array([w, h, w, h])
+            (startX, startY, endX, endY) = box.astype("int")
+
+            # ensure the bounding boxes fall within the dimensions of
+            # the frame
+            (startX, startY) = (max(0, startX), max(0, startY))
+            (endX, endY) = (min(w - 1, endX), min(h - 1, endY))
+
+            # extract the face ROI, convert it from BGR to RGB channel
+            # ordering, resize it to 224x224, and preprocess it
+            face = image[startY:endY, startX:endX]
+            face = cv2.cvtColor(face, cv2.COLOR_BGR2RGB)
+            face = cv2.resize(face, (224, 224))
+            face = img_to_array(face)
+            face = preprocess_input(face)
+            face = np.expand_dims(face, axis=0)
+
+            # pass the face through the model to determine if the face
+            # has a mask or not
+            (mask, withoutMask) = model.predict(face)[0]
+
+            # determine the class label and color we'll use to draw
+            # the bounding box and text
+            color = (0, 255, 0) 
+            
+            cv2.rectangle(image, (startX, startY), (endX, endY), color, 2)
+            opencv_image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+            
+    return opencv_image
+    
+# ----------------------------------------------------------------
+
+
+#---------------------------- Number Plate Detection ------------------------------------
 # Number Plate Detection with Haar Cascade 
 def doNumberPlateDetectionCascade(img):
     # Load the cascade
@@ -50,21 +103,38 @@ def doNumberPlateDetectionCascade(img):
     for (x,y,w,h) in carplate_rects: 
         cv2.rectangle(img, (x,y), (x+w,y+h), (255,0,0), 2) 
     return img
-    # cv2.imshow('NumberPlate Detection', img)
-    # cv2.waitKey()
-    
 
+#canny edge detection
+def cannyimg(image):
+    return cv2.Canny(image, 100, 200)
+
+#thresholding
+def thresholding(image):
+    return cv2.threshold(image, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)[1]
+
+# get grayscale image
+def get_grayscale(image):
+    return cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+
+#Grab number plate from image using easyocr
 def doANPR(img):
     
     # Convert int to uint -> np.array
     img = Image.fromarray((img * 255).astype(np.uint8))
     img = np.array(img)
-
+    
+    
+    gray = get_grayscale(img)
+    thresh = thresholding(gray)
+    canny = cannyimg(thresh)
+            
+    st.image(canny)
     #Perform OCR
-    reader = easyocr.Reader(['ch_sim','en']) 
+    reader = easyocr.Reader(['en']) 
         
-    result = reader.readtext(img)
-    st.subheader("Number Plate Digits :{}".format(result[0][1]))
+    result = reader.readtext(canny)
+    st.subheader("Number Plate Digits :{}".format(result))
 
     
     
@@ -120,16 +190,14 @@ def main():
         file_bytes = np.asarray(bytearray(uploaded_file.read()), dtype=np.uint8)
         original =  cv2.imdecode(file_bytes, 1)
         opencv_image = cv2.imdecode(file_bytes, 1)
-        
-        #Perform Face Detection 
-        image = doFaceDetection(opencv_image)
+    
+        #Face Detection with and without Mask 
+        image = doMaskOnOffDetection(opencv_image)
+        # st.image(image)
         
         # #Do Number Plate Detection 
         detections = doNumberPlateDetectionCascade(image)
-
-        # Now do something with the image! For example, let's display it:
-        st.image(detections, channels="BGR")
-        # st.image(opencv_image)
+        st.image(detections, channels="RGB")
         
         
         #Crop Number Plate Function Call 
